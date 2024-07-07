7dce918608413922f323366604dc2b9eee29ae0e
Artemij Rilev <artik200050@gmail.com>
2024-07-07 14:13:54 +0300

    adding employer class

---
+++ src/employer.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+
+class BaseEmployer(ABC):
+    """Abstract class for creating Employer from API"""
+    @abstractmethod
+    def __init__(self, identification, name, url, vacancies_url, open_vacancies):
+        self.id = identification
+        self.name = name
+        self.url = url
+        self.vacancies_url = vacancies_url
+        self.open_vacancies = open_vacancies
+
+    @classmethod
+    def cast_to_object_list(cls, data):
+        pass
+
+
+class Employer(BaseEmployer):
+    def __init__(self, identification, name, url, vacancies_url, open_vacancies):
+        super().__init__(identification, name, url, vacancies_url, open_vacancies)
+
+    @classmethod
+    def cast_to_object_list(cls, data):
+        """"convert response data in list of class Employer objects"""
+
+        employers_list = []
+        for i in data:
+            employer = cls(i.get('id'),
+                           i.get('name'),
+                           i.get('url'),
+                           i.get('vacancies_url'),
+                           i.get('open_vacancies'))
+            employers_list.append(employer)
+
+        return employers_list
