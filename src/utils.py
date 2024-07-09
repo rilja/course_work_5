0b0962624d642d143b2329ec7ba5df56a893162e
Artemij Rilev <artik200050@gmail.com>
2024-07-09 17:41:45 +0300

    create create_database function in utils

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+
+def create_database(database_name: str, params: dict):
+    """Создание базы данных и таблиц для сохранения данных о каналах и видео."""
+
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute(f"DROP DATABASE IF EXISTS {database_name}")
+    cur.execute(f"CREATE DATABASE {database_name}")
+
+    cur.close()
+    conn.close()
+# def save_data_to_database(data: list, db_name: str, params: dict) -> None:
+#     """Saving data to db"""
+#     pass
